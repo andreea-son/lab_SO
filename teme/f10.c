a016497e9f1127b77f6e56dd70f1d30f36f8b20d
Son Andreea <82153848+andreea-son@users.noreply.github.com>
2022-10-18 23:29:41 +0300

    Add files via upload

---
+++ teme/f10.c
@@ -0,0 +1,62 @@
+/*
+F10. (2 puncte) Scrieti un program care concateneaza mai multe fisiere sursa
+ intr-unul destinatie. Se va lansa sub forma:
+   concat   f1 + f2 + ... + fn   f
+ unde f1, ..., fn sunt fisierele sursa iar f este fisierul destinatie.
+ Daca n=1, va copia pe f1 in f.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+  if(argc < 3){
+    fprintf(stderr, "Utilizare: %s <fisier1> + <fisier2> + ... + <fisiern> <fisier>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  if(argc % 2 == 0){
+      fprintf(stderr, "Utilizare: %s <fisier1> + <fisier2> + ... + <fisiern> <fisier>\n", argv[0]);
+      exit(EXIT_FAILURE);
+  }
+  for(int i = 2; i < argc - 2; i += 2){
+    if(strcmp(argv[i], "+") != 0){
+      fprintf(stderr, "Utilizare: %s <fisier1> + <fisier2> + ... + <fisiern> <fisier>\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+  for(int i = 1; i < argc - 1; i += 2){
+    if(strcmp(argv[i], argv[argc - 1]) == 0){
+      fprintf(stderr, "Se accepta doar fisiere distincte de fisierul destinatie!\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  FILE *fp1;
+  fp1 = fopen(argv[argc - 1], "w");
+  if(fp1 == NULL)
+    {
+        perror(argv[argc - 1]);
+        exit(EXIT_FAILURE);
+    }
+
+  for(int i = 1; i < argc - 1; i += 2){
+    FILE *fp2;
+    fp2 = fopen(argv[i], "r");
+    if(fp2 == NULL)
+    {
+        perror(argv[i]);
+        exit(EXIT_FAILURE);
+    }
+    while(!feof(fp2))
+    {
+        char ch;
+        if(fread(&ch, sizeof(char), 1, fp2) != 0)
+            fwrite(&ch, sizeof(char), 1, fp1);
+    }
+    fclose(fp2);
+  }
+
+  fclose(fp1);
+
+  return 0;
+}
---
+++ teme/f3.c
@@ -0,0 +1,59 @@
+//F3. (2.5 puncte) Scrieti un program care primeste ca argument in linia de
+//comanda un intreg si afisaza descompunerea sa in factori primi.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    if(argc != 2){
+        fprintf(stderr, "Utilizare: %s <numar pozitiv>\n", argv[0]);
+        return 1;
+    }
+    
+    char *str = NULL;
+    long nr = strtol(argv[1], &str, 10);
+    
+    if(*argv[1] == '\0' || *str != '\0'){
+        fprintf(stderr, "%s nu este un numar, contine %s\n", argv[1], str);
+        return 2;
+    }
+
+    if(nr < 0){
+        fprintf(stderr, "%ld nu este un numar pozitiv, nu se poate calcula descompunerea in factori primi\n", nr);
+        return 3;
+    }
+
+    long i = 1;
+    long j;
+
+    printf("descompunere in factori primi pentru %ld:\n", nr);
+
+    if(nr < 2){
+        printf("%ld = %ld", nr, nr);
+    }
+    else{
+        printf("%ld = ", nr);
+
+        while(nr != 1){
+            ++i;
+            j = 0;
+            while(nr % i == 0){
+                nr = nr / i;
+                ++j;
+            }
+            if(j != 0){
+                if(j > 1){
+                    printf("(%ld ^ %ld) ", i, j);   
+                }
+                else{
+                    printf("%ld ", i);
+                }
+                if(nr != 1){
+                    printf("* ");
+                }
+            } 
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
